38aeb7e0904a54c7870d8a57aad24fadbd2ac3d6
Xinxin Li <xinxinidea@gmail.com>
2025-03-21 10:43:27 -0700

    index.html

---
+++ scripts.js
@@ -0,0 +1,69 @@
+// Toggle light / dark modes
+document.getElementById("toggleLightDark").addEventListener("click", () => {
+
+    document.getElementsByTagName("body")[0].classList.toggle("bodyDark");
+
+    document.getElementById("oval").classList.toggle("ovalDark");
+
+    document.getElementById("nav").classList.toggle("nav-dark");
+
+    let sun = document.getElementById("sun");
+    if (sun) {
+        if (window.getComputedStyle(sun, null).getPropertyValue("display") == "none") {
+            sun.style.display = "block";
+        } else {
+            sun.style.display = "none";
+        }
+    }
+
+    let moon = document.getElementById("moon");
+    if (moon) {
+        if (window.getComputedStyle(moon, null).getPropertyValue("display") == "none") {
+            moon.style.display = "block";
+        } else {
+            moon.style.display = "none";
+        }
+    }
+
+    let penguinLight = document.getElementById("penguin-light");
+    if (penguinLight) {
+        if (window.getComputedStyle(penguinLight, null).getPropertyValue("display") == "none") {
+            penguinLight.style.display = "block";
+        } else {
+            penguinLight.style.display = "none";
+        }
+    }
+
+    let penguinDark = document.getElementById("penguin-dark");
+    if (penguinDark) {
+        if (window.getComputedStyle(penguinDark, null).getPropertyValue("display") == "none") {
+            penguinDark.style.display = "block";
+        } else {
+            penguinDark.style.display = "none";
+        }
+    }
+
+    let titleSectionsLight = document.getElementsByClassName("section-title-container-light");
+    if (titleSectionsLight) {
+        for (let i = 0; i < titleSectionsLight.length; i++) {
+            console.log(window.getComputedStyle(titleSectionsLight.item(i)).getPropertyValue("display"));
+            if (window.getComputedStyle(titleSectionsLight.item(i)).getPropertyValue("display") == "none") {
+                titleSectionsLight.item(i).style.display = "flex";
+            } else {
+                titleSectionsLight.item(i).style.display = "none";
+            }
+        }
+    }
+
+    let titleSectionsDark = document.getElementsByClassName("section-title-container-dark");
+    if (titleSectionsDark) {
+        for (let i = 0; i < titleSectionsDark.length; i++) {
+            if (window.getComputedStyle(titleSectionsDark.item(i)).getPropertyValue("display") == "none") {
+                titleSectionsDark.item(i).style.display = "flex";
+            } else {
+                titleSectionsDark.item(i).style.display = "none";
+            }
+        }
+    }
+
+});
